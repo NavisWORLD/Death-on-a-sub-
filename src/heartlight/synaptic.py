0c9106aa60a48368e61fc1745e001f706013bd4f
NavisWORLD <personal.phera@gmail.com>
2026-08-13 10:49:12 -0500

    Resolve Python lint import

--- src/heartlight/synaptic.py
+++ src/heartlight/synaptic.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import math
+from collections.abc import Iterable
 from dataclasses import dataclass
-from typing import Iterable
 
 SYNAPTIC_KERNEL_VERSION = "1.0"
 

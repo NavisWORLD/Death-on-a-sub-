d9787812580d8613d4e2e6450835c339b131b91a
NavisWORLD <personal.phera@gmail.com>
2026-08-13 00:38:03 -0500

    Add Kotlin HEARTLIGHT SDK

---
+++ sdk/kotlin/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    testImplementation(kotlin("test"))
+}
+
+kotlin { jvmToolchain(17) }
